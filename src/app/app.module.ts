2b03c4478783e1732426606d96549b63579ffcf7
JavierRamosVillarejo <javierramosvillarejo@gmail.com>
2021-05-05 11:22:54 +0200

    primera subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { FormsModule } from '@angular/forms';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { LoteriaComponent } from './componentes/loteria/loteria.component';
+import { GenerarLoteriaComponent } from './componentes/generar-loteria/generar-loteria.component';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
+import { MultiplicarComponent } from './componentes/multiplicar/multiplicar.component';
+import { HolaComponent } from './componentes/hola/hola.component';
+import { EstructurasComponent } from './componentes/estructuras/estructuras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavegacionComponent,
+    LoteriaComponent,
+    GenerarLoteriaComponent,
+    CalculadoraComponent,
+    MultiplicarComponent,
+    HolaComponent,
+    EstructurasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/estructuras/estructuras.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estructuras',
+  templateUrl: './estructuras.component.html',
+  styleUrls: ['./estructuras.component.css']
+})
+export class EstructurasComponent implements OnInit {
+
+  verdad: boolean= false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
